783932ab60e3d85db65db964cdcb47b21a3d6984
Wen <2579100977@qq.com>
2026-05-10 12:46:30 +0000

    updated to the start of hms_counter.sv

---
+++ rtl/hms_counter.sv
@@ -0,0 +1,18 @@
+module hms_counter #(
+    parameter int N_HOURS   = 24,  // number of hours
+    parameter int N_MINUTES = 60,  // number of minutes
+    parameter int N_SECONDS = 60,  // number of seconds
+
+    // Output port widths
+    parameter int W_HOURS   = 5,
+    parameter int W_MINUTES = 6,
+    parameter int W_SECONDS = 6
+) (
+    input logic clk,
+    input logic enable,
+    output logic [W_HOURS-1:0] hours,
+    output logic [W_MINUTES-1:0] minutes,
+    output logic [W_SECONDS-1:0] seconds
+);
+
+endmodule
